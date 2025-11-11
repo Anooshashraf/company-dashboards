c2eb02e6b7e0fce8b9ab56d6088e391e37f824ab
Anooshashraf <ASHRAF4621021@cloud.neduet.edu.pk>
2025-11-11 14:08:45 -0600

    ..

--- company-dashboards/src/app/login/page.tsx
+++ company-dashboards/src/app/login/page.tsx
@@ -149,7 +149,3 @@ export default function Login() {
     </div>
   );
 }
-
-
-
-
---
+++ company-dashboards/src/lib/users.ts
@@ -0,0 +1,28 @@
+export const DEFAULT_USERS = [
+    {
+        email: "fahad@techno-communications.com",
+        password: "fahad123",
+        role: "Manager",
+    },
+    { email: "GHANI@TEXASMOBILEPCS.COM", password: "ghani123", role: "Auditor" },
+    {
+        email: "aleem.ghori@techno-communications.com",
+        password: "aleem123",
+        role: "Analyst",
+    },
+    {
+        email: "hasnain.mustaqeem@techno-communications.com",
+        password: "hasnain123",
+        role: "Viewer",
+    },
+    {
+        email: "inventory2025@gmail.com",
+        password: "inventory2025",
+        role: "Viewer",
+    },
+    {
+        email: "nasim@techno-communications.com",
+        password: "nasim123",
+        role: "Viewer",
+    },
+];
